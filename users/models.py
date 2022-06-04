77a20f840ffdf0defdeb6758f53e274d6753928b
aust1inn <austinotieno364@gmail.com>
2022-06-04 21:53:56 +0300

    sets up page for updating user profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 
 
 # Create your models here.
@@ -22,6 +24,15 @@ class Profile(models.Model):
         profile = Profile.objects.filter(user__username__icontains=search_term)
         return profile
 
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
 
 class Image(models.Model):
