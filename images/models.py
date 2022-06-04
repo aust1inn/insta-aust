a0e0a0f0e9471cd6e452b294cf2c12b9f6e779c7
aust1inn <austinotieno364@gmail.com>
2022-06-04 12:30:33 +0300

    updates user profile

--- images/models.py
+++ images/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
-    profile_photo= models.ImageField(upload_to='profiles/',null=True)
+    profile_photo= models.ImageField(upload_to='profiles/',null=True,default='default.jpg')
     bio= models.CharField(max_length=240, null=True)
 
 
