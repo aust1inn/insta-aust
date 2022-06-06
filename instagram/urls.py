810b66c74d3d2afa0a902d91782ce76175e998f6
aust1inn <austinotieno364@gmail.com>
2022-06-06 13:16:19 +0300

    sets html markup for the search.html page

--- instagram/urls.py
+++ instagram/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('search/', user_views.search_user, name='search'),
+    path('search/', user_views.search_users, name='search'),
 
 ]
