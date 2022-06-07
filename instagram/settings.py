1fec01327aca2896ce636ebb851aecebf32f5dff
aust1inn <austinotieno364@gmail.com>
2022-06-07 14:31:04 +0300

    deployment

--- instagram/settings.py
+++ instagram/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '02@sxtrk8922b#3ysn4g@98'
+SECRET_KEY = 'django-insecure-#vhj4$a2boj*ou3mva4f_1q2znr79@z#p%m)9nt0_-z7#fub=@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
